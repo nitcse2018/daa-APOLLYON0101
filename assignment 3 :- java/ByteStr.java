f620076e365171f83c71232da2631b10fee3c981
APOLLYON0101 <63863756+APOLLYON0101@users.noreply.github.com>
2020-05-10 22:01:05 +0530

    Create ByteStr.java

---
+++ assignment 3 :- java/ByteStr.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class ByteStr{
+	public static void main(String[] args) throws IOException {
+		// reading data from the file
+		FileInputStream file = new FileInputStream("F:\\eclipse\\first\\file1.txt");
+		int data;
+		try {
+			while((data = file.read()) != -1) {
+				System.out.print((char) data);
+			}
+			System.out.println();
+			file.close();
+		}catch(IOException e) {
+			System.out.println("Error in reading the file");
+			e.printStackTrace();
+		}
+		
+		// writing data in a file
+		try{
+			FileOutputStream writer = new FileOutputStream("F:\\eclipse\first\\file2.txt");
+			String info = "I am adding some new data";
+			byte[] convert = info.getBytes();
+			writer.write(convert);
+			writer.close();
+			System.out.println("Successfully wrote to the file");
+		}catch(IOException e) {
+			System.out.println("Error in writing content to the file");
+			e.printStackTrace();
+		}
+}
+}
